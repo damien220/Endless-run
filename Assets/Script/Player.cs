88ba811bfc1cf0aae72dba30cb5fd48b4aa65b1c
Damien220 <ashrafalnas@hotmail.com>
2018-04-04 06:06:28 +0300

    Initial Commit,Setting up the game for use,first run of the player

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	private Rigidbody rb;
+	[SerializeField]
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody>();
+		rb.velocity = new Vector3(0f,0f,speed);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
